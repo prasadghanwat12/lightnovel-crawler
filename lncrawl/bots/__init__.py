9c8ae9be687ed81148702955b114c8314ebf1180
prasadghanwat12 <ruchitaza6@gmail.com>
2024-10-21 17:30:59 +0530

    Update __init__.py

--- lncrawl/bots/__init__.py
+++ lncrawl/bots/__init__.py
@@ -5,26 +5,28 @@ supported_bots = [
     "lookup",
 ]
 
-
 def run_bot(bot):
     if bot not in supported_bots:
         bot = "console"
 
     if bot == "console":
         from ..bots.console import ConsoleBot
-
         ConsoleBot().start()
+
     elif bot == "telegram":
         from ..bots.telegram import TelegramBot
+        
+        # Create an instance of TelegramBot and run it
+        telegram_bot = TelegramBot()
+        telegram_bot.run()  # Use the run method instead of start
 
-        TelegramBot().start()
     elif bot == "discord":
         from ..bots.discord import DiscordBot
-
         DiscordBot().start_bot()
+
     elif bot == "lookup":
         from ..bots.lookup import LookupBot
-
         LookupBot().start()
+
     else:
         print("Unknown bot: %s" % bot)
